d75b945e1694e3dd746995152b6245f4365693cc
andres <andres.chavex@gmail.com>
2013-07-21 22:25:33 -0400

    project startup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cemla::Application.config.secret_token = 'ac88f6d75f2729fa67f95985fbe602f429238930a264e00fa44dd89c7444dd91e0a575a3b40397f8839e0119a26d2c23f0a2f926d1718021fba7381c91f26ba0'
